07f4337722261fba47c4e1e89ad8e71aab366bc5
KittipopPhetburi <s6506021620016@email.kmutnb.ac.th>
2023-12-06 16:38:48 +0700

    Save

---
+++ Example1.js
@@ -0,0 +1,10 @@
+function findPerimeter(l,w){
+    return 2*l+2*w;
+}
+
+const l = 6;
+const w = 7;
+
+const result = findPerimeter(l,w);
+
+console.log(result);
---
+++ Example2.js
@@ -0,0 +1,8 @@
+const a = "1+1";
+const b = a.split("+");
+function equation(a){
+    
+    return b[0]+b[1];
+}
+
+console.log(b);
